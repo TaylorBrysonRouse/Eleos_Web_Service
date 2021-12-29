e06ecbe616b444a5794f73a9d04d8fb848368b08
Taylor Rouse <taylor.rouse@eleostech.com>
2021-12-29 11:09:45 -0500

    Config - Holds Database URI

--- config/dbkey.js
+++ config/dbkey.js
@@ -1,3 +1,1 @@
-module.exports = {
-    mongoURI: 'mongodb+srv://Taylor2000:Taylor2000@webservicedb.wcwgx.mongodb.net/WebServiceDB?retryWrites=true&w=majority'
-};
+module.exports = { mongoURI: 'mongodb+srv://Taylor2000:Taylor2000@webservicedb.wcwgx.mongodb.net/WebServiceDB?retryWrites=true&w=majority' };
